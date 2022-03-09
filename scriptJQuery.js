7b8ce956b9b933a95f718495d47652ca4d1ea521
AliannaFormoso <60233117+AliannaFormoso@users.noreply.github.com>
2022-03-09 17:04:44 +0100

    Add files via upload

---
+++ scriptJQuery.js
@@ -0,0 +1,30 @@
+/* eslint-env jquery */
+
+$(document).ready(function () {
+  $("header").css(
+    "background-image",
+    "url(https://upload.wikimedia.org/wikipedia/commons/6/65/Shades_of_orange.png)"
+  );
+  $("header").css("margin", "15px");
+  $("header").css("color", "white");
+  $("header").css("opacity", "0.6");
+  $("header").css("box-shadow", "0 20px 10px brown, 0 6px 16px brown");
+  $("header").css("border-radius", "25px");
+  $("header").css("border", "5px dotted brown");
+  $("header").css("height", "130px");
+  $("header").css("padding", "0px 25px");
+  $("header button").css("float", "right");
+  $(":header").css("font-weight", "bold");
+  $(":header").css("text-transform", "uppercase");
+  $(":header").css("text-align", "center");
+  $("main").css("background-color", "beige");
+  $("main").css("margin", "50px");
+  $("main").css("padding", "50px");
+  $("main").css("border-radius", "35px 0px 35px 0px");
+  $("footer").css("border-radius", "0px 0px 25px 25px");
+  $("footer").css("padding", "15px");
+  $("footer").css("background-color", "#c94831");
+  $("footer").css("margin", "15px");
+  $("footer").css("color", "white");
+  $("footer p").css("text-align", "center");
+});
